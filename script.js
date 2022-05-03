45d7ffc58884a5889b67c658fb63ca3c01d3d687
Yourengab <yourenw1@gmail.com>
2022-05-03 14:40:13 +0800

    cari lokasi

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const nav = document.querySelector("nav ul");
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+const vaksinToggle = document.querySelector(".vaksin button");
+const vaksinSearch = document.querySelector(".vaksin .vaksin-search");
+const closeSearch = document.querySelector(".vaksin-search .close");
+vaksinToggle.addEventListener("click", function () {
+  vaksinSearch.classList.toggle("show");
+});
+closeSearch.addEventListener("click", function () {
+  vaksinSearch.classList.toggle("show");
+});
+const vaksinClose = document.querySelector(".main-search .btn");
+vaksinClose.addEventListener("click", function () {
+  alertBar.classList.toggle("alert-show");
+});
+const alertBar = document.querySelector(".vaksin-search .alert");
+const alertClose = document.querySelector(".vaksin-search .close-alert");
+alertClose.addEventListener("click", function () {
+  alertBar.classList.toggle("alert-show");
+});
